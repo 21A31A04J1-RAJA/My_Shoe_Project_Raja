6ac711ebe71cd11594dcb9bc10cc9ed634434509
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-10-17 23:25:22 +0200

    feat: Add banner and footer components
    - Create "Banner" component to display the manufacturing process.
    - Create "Footer" component to display important links and social media.
    - Update "Home" page to include the banner.
    - Update app.component.html to include footer component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import {
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ShopComponent } from './pages/shop/shop.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, RouterLinkActive],
+  imports: [RouterOutlet, NavbarComponent, RouterLinkActive, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
