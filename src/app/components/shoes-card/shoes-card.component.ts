aac1705c471228e0701d60005537ef4631b0aaf6
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-10-18 22:43:47 +0200

    feat: Add shoes card component
    - Add shoes card component to display shoes information in the shop page.

---
+++ src/app/components/shoes-card/shoes-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoes-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './shoes-card.component.html',
+  styleUrl: './shoes-card.component.scss'
+})
+export class ShoesCardComponent {
+
+}
