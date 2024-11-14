73c6e7dca8a2d2cdf8d12c7eae460e9cd37ce71f
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-11-14 23:41:15 +0100

    feat: add SignupForm and LoginForm.
    - Implements SignupForm and LoginForm in My Account page.

--- src/app/pages/myaccount/myaccount.component.ts
+++ src/app/pages/myaccount/myaccount.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-myaccount',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './myaccount.component.html',
-  styleUrl: './myaccount.component.scss'
+  styleUrl: './myaccount.component.scss',
 })
 export class MyaccountComponent {
+  toggleForm: boolean = true;
 
+  loginInForm = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  signUpForm = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    name: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+  });
+
+  loginSubmit() {
+    this.loginInForm.get('email')?.markAsTouched();
+    this.loginInForm.get('password')?.markAsTouched();
+    console.log(this.loginInForm.value);
+    console.log(this.loginInForm.status);
+  }
+
+  signUpSubmit() {
+    this.signUpForm.get('email')?.markAsTouched();
+    this.signUpForm.get('password')?.markAsTouched();
+    this.signUpForm.get('name')?.markAsTouched();
+    console.log(this.signUpForm.value);
+    console.log(this.signUpForm.status);
+  }
+
+  switchForm() {
+    this.toggleForm = !this.toggleForm;
+  }
 }
